c2e2069f70e5499f4c4354d19111997c4998b9fa
tryle17 <langhuoshao@gmail.com>
2025-12-30 08:50:58 +0800

    fix: 优顺增加更新URL

--- youshun.js
+++ youshun.js
@@ -7,6 +7,8 @@
 // @match        https://agent.oriental-sky.com/*
 // @grant        none
 // @run-at       document-idle
+// @updateURL    https://raw.githubusercontent.com/tryle17/yuemeihua-scripts/main/youshun.js
+// @downloadURL  https://raw.githubusercontent.com/tryle17/yuemeihua-scripts/main/youshun.js
 // ==/UserScript==
 
 /*
